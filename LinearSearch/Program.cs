0a285ab7f94d8f792aa17e8f121862019659b806
eihap <eihapinfo@gmail.com>
2023-05-15 15:19:21 +0300

    update program file

--- LinearSearch/Program.cs
+++ LinearSearch/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace LinearSearch
+﻿using Microsoft.Extensions.DependencyInjection;
+using Bl;
+using LinearSearch.Utlities;
+using Domains;
+using Bl.Services;
+using Bl.Interfaces;
+
+namespace LinearSearch
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region dependency injection
+            // Create a new instance of the ServiceCollection class
+            var services = new ServiceCollection();
+
+            // Register the required services and their implementations
+            services.AddScoped<Bl.Interfaces.IBusinessLayer<TbNumber>, NumberService>();
+            // Build the service provider
+            var serviceProvider = services.BuildServiceProvider();
+            #endregion
+
+                #region instance of UiHelper 
+                UiHelper uiHelper = new UiHelper
+        (
+            serviceProvider.GetService<IBusinessLayer<TbNumber>>(),
+            new TbNumber()
+
+         );
+                #endregion
+            
         }
     }
 }
