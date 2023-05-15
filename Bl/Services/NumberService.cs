091d68c9d80c561a9271cfbdc4792e8322ca0184
eihap <eihapinfo@gmail.com>
2023-05-15 14:13:52 +0300

    create NumberService to Bl lib in Services folder

---
+++ Bl/Services/NumberService.cs
@@ -0,0 +1,17 @@
+﻿using Bl.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bl.Services
+{
+    public class NumberService : IBusinessLayer
+    {
+        public int LinearSearch()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
